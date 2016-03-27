17c54e839c589c6b6bc576884783ff22e97d7c07
Konstantin Davidenko <kos.davidenko@gmail.com>
2016-03-27 17:28:03 +0300

    - Moved controller initialization on window load event

--- FHN_nonlocal_coupling/View/WindowPDE.cs
+++ FHN_nonlocal_coupling/View/WindowPDE.cs
@@ -13,11 +13,11 @@ namespace FHN_nonlocal_coupling.View
         public WindowPDE()
         {
             InitializeComponent();
-            controller = new PDEController(new ViewElements(chart, null, propertyGrid1, propertyGrid2, prBarSolve, trBarT));
         }
 
         private void WindowPDE_Load(object sender, EventArgs e)
         {
+            controller = new PDEController(new ViewElements(chart, null, propertyGrid1, propertyGrid2, prBarSolve, trBarT));
             controller.reallocate(checkBox2ndEq.Checked);
         }
 
